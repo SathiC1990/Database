0298447defc7e14d24b062292ed1312ac90aa1b7
sathic <sathichowdhury2017@gmail.com>
2024-09-13 09:20:42 +0200

    Meal

--- DB week3/meal.sql
+++ DB week3/meal.sql
@@ -52,4 +52,37 @@ CREATE TABLE Meal (
     -> ('Fresh Sushi!', 'Loved the Sushi Night, everything was very fresh and tasty.', 3, 5, '2024-09-23');
 	
 	
+	--Meal
+	--get all meal:
+	 select * from meal;
+	 +----+----------------+---------------------------------------------+---------------------+---------------------+------------------+-------+--------------+
+| id | title          | description                                 | location            | when                | max_reservations | price | created_date |
++----+----------------+---------------------------------------------+---------------------+---------------------+------------------+-------+--------------+
+|  1 | Italian Dinner | A classic Italian meal with pasta and wine  | Rome, Italy         | 2024-09-20 18:30:00 |               50 | 29.99 | 2024-09-10   |
+|  2 | Mexican Fiesta | Authentic Mexican dishes with spicy options | Mexico City, Mexico | 2024-09-21 19:00:00 |               60 | 24.99 | 2024-09-10   |
+|  3 | Sushi Night    | Fresh sushi from Japan with sake            | Tokyo, Japan        | 2024-09-22 20:00:00 |               40 | 34.99 | 2024-09-10   |
++----+----------------+---------------------------------------------+---------------------+---------------------+------------------+-------+--------------+
+	 
+	 --add a meal:
+	 INSERT INTO Meal (title, description, location, `when`, max_reservations, price, created_date)
+VALUES 
+('French Gourmet Night', 'A luxurious French meal featuring fine wines and gourmet dishes', 'Paris, France', '2024-09-25 19:00:00', 30, 49.99, '2024-09-10');
+
+
+
+
+select * from Meal where id = 2;
+
+
+UPDATE Meal
+    -> SET title = 'Italian Gourmet Dinner', 
+    ->     price = 39.99,
+    ->     location = 'Venice, Italy'
+    -> WHERE id = 1;
+	
+	--Delete a meal with any id, fx 1
+	alter table Reservation DROP FOREIGN KEY Reservation_ibfk_1;    
+	alter table Reservation
+    -> add constraint Reservation_ibfk_1
+    -> FOREIGN KEY (meal_id) REFERENCES Meal(id) ON DELETE CASCADE;
 	
